8bde255e05c9d40e30e06f5c1656d498a7589e85
dsl2055 <job2019tmm@gmail.com>
2022-12-18 13:22:49 -0500

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isTicking, setIsTicking] = useState(true);
+  const [payment, setPayment] = useState(0);
+
+  function handler() {
+    setPayment(payment + 10);
+    setIsTicking(!isTicking);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{`you have paid $${payment}`}</div>
+      {isTicking ? (
+        <img
+          src="https://cdn.dribbble.com/users/158127/screenshots/3972213/media/c76e1fd4afa2f69b3feb0ab234da6ed2.gif"
+          alt="bomb-ticking"
+        />
+      ) : (
+        <img
+          src="https://cdn.dribbble.com/users/1179280/screenshots/5704456/media/934a8bc167ec6c37dde823c7fe08f000.gif"
+          alt="bomb-exploding"
+        />
+      )}
+      <button disabled={!isTicking} onClick={handler}>
+        Switch
+      </button>
+      <button onClick={handler}>Pay 10 dollars</button>
     </div>
   );
 }
